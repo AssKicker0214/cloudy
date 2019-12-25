56ccd51c236e31f2698932dc560fbdb3f44bf8b6
assk <asskicker0214@outlook.com>
2019-12-26 00:08:47 +0800

    can be browsed

--- src/main/resources/static/component/file-list.js
+++ src/main/resources/static/component/file-list.js
@@ -24,19 +24,31 @@ Vue.component("file-entry", {
             return this.attributes.type === 'd' ? '-' : this.attributes.sizeForHuman;
         },
         time(){
+            if (this.attributes.modified) {
             let date = new Date(this.attributes.modified);
-            console.log(date);
             return `${date.getFullYear()}/${date.getMonth()}/${date.getDate()} ${date.getHours()}:${date.getMinutes()}`
+            }else{
+                return '-';
+            }
         }
     },
     template: `
     <li class="file-entry">
         <i :class="['fas', icon]"></i>
-        <span class="file-name">{{ attributes.name }}</span>
+        <span class="file-name" @click="clickName(attributes.name, attributes.type)">{{ attributes.name }}</span>
         <span>{{ size }}</span>
         <span>{{ time }}</span>
     </li>
-    `
+    `,
+    methods: {
+          clickName(name, type){
+              if (type === 'd') {
+                  this.$emit("enter-directory", name);
+              }else if (type === '-') {
+                  this.$emit("download-file", name);
+              }
+          }
+    }
 });
 
 
@@ -44,12 +56,27 @@ Vue.component("file-list", {
     props: {
         entries: Array
     },
+    computed: {
+        _entries(){
+            return [{type: 'd', name: '..'}].concat(this.entries);
+        }
+    },
 
     template:
         `
     <ul class="file-list">
-        <file-entry v-for="(entry, i) in entries" :key="'entry-'+i" :attributes="entry"></file-entry>
+        <file-entry v-for="(entry, i) in _entries" :key="'entry-'+(i+1)" :attributes="entry" 
+        @enter-directory="enterDirectory" @download-file="downloadFile">
+        
+        </file-entry>
     </ul>
     `,
-    methods: {}
+    methods: {
+        enterDirectory(name) {
+            this.$emit("enter-directory", name);
+        },
+        downloadFile(name) {
+            this.$emit("download-file", name);
+        }
+    }
 });
